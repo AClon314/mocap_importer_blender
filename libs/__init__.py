043e563f8cbbc8523a1f527180b58ac95fae70ea
AClon314 <30747832+AClon314@users.noreply.github.com>
2025-09-03 07:51:35 +0800

    fix(init): 将 FKtoIK 导入移入 try 块以处理 ImportError

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,16 +1,27 @@
-from .FKtoIK import fktoikaddon
 from ..logger import Log
 from ..lib import Progress, GEN
-fktoikaddon.Log = Log
-fktoikaddon.Progress = Progress
-fktoikaddon.GEN = GEN
+
 # IS_DEBUG = bpy.context.preferences.view.show_developer_ui
-from .FKtoIK.fktoikaddon import gen_fk_to_ik
-REG = [
-    fktoikaddon.register
-]
-UNREG = [
-    fktoikaddon.unregister
-]
-def register(): [reg() for reg in REG]
-def unregister(): [unreg() for unreg in UNREG]
+try:
+    from .FKtoIK import fktoikaddon
+
+    fktoikaddon.Log = Log
+    fktoikaddon.Progress = Progress
+    fktoikaddon.GEN = GEN
+    from .FKtoIK.fktoikaddon import gen_fk_to_ik
+
+    REG = [fktoikaddon.register]
+    UNREG = [fktoikaddon.unregister]
+
+    def register():
+        [reg() for reg in REG]
+
+    def unregister():
+        [unreg() for unreg in UNREG]
+
+except ImportError:
+    import os
+
+    Log.warning(
+        f"Get FKtoIK at https://github.com/AClon314/FKtoIK/tree/main, then place under {os.path.join(os.path.dirname(__file__), 'FKtoIK')}"
+    )
